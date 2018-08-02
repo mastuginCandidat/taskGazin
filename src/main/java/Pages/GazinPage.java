2d71b72f69511243b4e9f3932f09768a123edff8
mastuginCandidat <42031140+mastuginCandidat@users.noreply.github.com>
2018-08-02 13:01:48 +0300

    Update .gitignore

---
+++ src/main/java/Pages/GazinPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import com.codeborne.selenide.Condition;
+
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GazinPage {
+
+    public void waitGazinPage() {
+        $(byClassName("about__wrapper")).shouldBe(Condition.exist);
+    }
+}
